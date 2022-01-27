52a0c2e037c815b386d10f1758b0c71066d542f7
arisa.pan <arisa.pan@mfec.co.th>
2022-01-27 15:35:01 +0700

    implement logic which is century year divided by 4 is leap year

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,6 @@
 def is_a_leap_year(year):
     if year % 100 == 0 and year % 400 == 0:
         return True
-    if year % 100 != 0:
+    if year % 100 != 0 and year % 4 == 0:
         return True
     return False
